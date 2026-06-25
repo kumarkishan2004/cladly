f10118ee5b0d7f32ca5ac60f689d4472b6e3a09d
unknown <kishankumarsahoo450@gmail.com>
2026-06-26 01:04:48 +0530

    connect to postgresql database and update careers pages

--- cladly/settings.py
+++ cladly/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'cladly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cladlydb',
+        'USER': 'postgres',
+        'PASSWORD': 'Sahoo@2004',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
